cfa551103f07f2c1dfb946f25c1653fc7032528a
Jessy Catterwaul <jessy@catterwaul.com>
2024-08-08 11:27:20 -0400

    Create package

---
+++ Package.swift
@@ -0,0 +1,67 @@
+// swift-tools-version: 6.0
+
+import Foundation // for String.capitalized
+import PackageDescription
+
+let name = "Cast"
+
+_ = Package(
+  name: name,
+  platforms: [.iOS(.v13), .macOS(.v10_15)],
+  products: [.library(name: name, targets: [name])],
+  dependencies: [
+    .package(url: "https://github.com/swiftlang/swift-docc-plugin", branch: "main")
+  ],
+  targets: [
+    .target(name: name),
+    .testTarget(
+      name: name + ".Tests",
+      dependencies: [.init(stringLiteral: name)]
+    )
+  ]
+)
+
+// MARK: - Dependency
+
+nonisolated var dependencies: [Dependency]  {
+  [ .swift(repositoryName: "docc-plugin")
+  ]
+}
+
+struct Dependency {
+  let package: Package.Dependency
+  let product: Target.Dependency
+}
+
+extension Dependency {
+  static func apple(repositoryName: String) -> Self {
+    .swift(organization: "apple", repositoryName: repositoryName)
+  }
+
+  static func swift(organization: String = "swiftlang", repositoryName: String) -> Self {
+    .init(
+      organization: organization,
+      name: repositoryName.split(separator: "-").map(\.capitalized).joined(),
+      repositoryName: "swift-\(repositoryName)"
+    )
+  }
+
+  static func catterwaul(name: String, repositoryName: String? = nil, branch: String? = nil) -> Self {
+    .init(
+      organization: "Catterwaul",
+      name: name,
+      repositoryName: repositoryName ?? name,
+      branch: branch
+    )
+  }
+
+  private init(organization: String, name: String, repositoryName: String, branch: String? = nil) {
+    self.init(
+      package: .package(
+        url: "https://github.com/\(organization)/\(repositoryName)",
+        branch: branch ?? "main"
+      ),
+      product: .product(name: name, package: repositoryName)
+    )
+  }
+}
